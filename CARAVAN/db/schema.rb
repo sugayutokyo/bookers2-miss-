e8061c22097aa0f659f6bdbda1bc4f79884c6ba4
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2018-02-02 17:31:32 +0900

    Finish 4-1

--- CARAVAN/db/schema.rb
+++ CARAVAN/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180106070015) do
+ActiveRecord::Schema.define(version: 20180106070228) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
