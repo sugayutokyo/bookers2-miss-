a6366ad35cd94c95f46c4c443c822504fac6be14
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2018-03-04 18:08:20 +0900

    2018/3/4 Sun End

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :books
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
